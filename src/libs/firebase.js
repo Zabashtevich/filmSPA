a6bcdb47aeb5ac2cd1a558a001a77178b4830d98
Zabashtevich <vlados.zabashta@mail.ru>
2020-12-21 21:31:45 +0300

    add: init firebase to project

---
+++ src/libs/firebase.js
@@ -0,0 +1,16 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };

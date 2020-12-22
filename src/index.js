c58fa0da2049243b3cf8c448ef252f225b00e822
Zabashtevich <vlados.zabashta@mail.ru>
2020-12-22 08:13:24 +0300

    add: firebase context to app

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import "normalize.css";
 import App from "./App";
 import { GlobalStyles } from "./theme/global-style";
 import Theme from "./theme/theme";
+import { firebase } from "./libs/firebase";
+import { AuthContext } from "./context/auth-context";
 
 ReactDOM.render(
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
-    <App />
+    <AuthContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </AuthContext.Provider>
   </ThemeProvider>,
   document.getElementById("root"),
 );
--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import HeaderMainContainer from "../containers/header-container";
 import BodyMainContainer from "../containers/body-main-page-container";
 import FooterContainer from "../containers/footer-container";
+import { AuthContext } from "../context/auth-context";
 
 export default function MainPage() {
+  const { firebase } = useContext(AuthContext);
+  console.log(firebase);
   return (
     <>
       <HeaderMainContainer />

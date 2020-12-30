eafe80c262f83f7e4aaed99158311aead8c15477
Zabashtevich <vlados.zabashta@mail.ru>
2020-12-31 02:56:41 +0300

    add: custom hook useFirestore

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,31 @@
+import { useContext, useState, useEffect } from "react";
+
+import { AuthContext } from "../context/auth-context";
+
+export default function useFirestore({ collectionTarget, docTarget }) {
+  const [data, setData] = useState(null);
+  const [gettingData, setGettingData] = useState(false);
+
+  const { firebase } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (gettingData) return;
+    firebase
+      .collection(`${collectionTarget}`)
+      .doc(`${docTarget}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log(doc);
+          setData(doc);
+        } else {
+          setData([]);
+        }
+      });
+    return () => {
+      setGettingData(true);
+    };
+  }, []);
+
+  return { data };
+}

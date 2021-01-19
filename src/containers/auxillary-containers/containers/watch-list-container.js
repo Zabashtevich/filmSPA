e9c4fb53b6ff02ccab651fe8261fa48174e728fa
Zabashtevich <vlados.zabashta@mail.ru>
2021-01-20 00:23:57 +0300

    refactor: add watch list aux items container

--- src/containers/auxillary-containers/containers/watch-list-container.js
+++ src/containers/auxillary-containers/containers/watch-list-container.js
@@ -4,30 +4,22 @@ import { CSSTransition } from "react-transition-group";
 import { WatchList } from "../../../components";
 import ErrorModalContainer from "./error-modal-container";
 import { AuthContext } from "../../../context/auth-context";
-
 import { createListLogic } from "../../../utils/firebase";
 import useFirestore from "./../../../hooks/useFirestore";
+import {
+  WatchListCreateItemContainer,
+  WatchListItemContainer,
+} from "./watch-list-items-container";
 
 export default function WatchListContainer({ user, watchListPopupVisible }) {
   const [dataLoading, data] = useFirestore(user.displayName, "collection");
-  const [inputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [dataSubmiting, setDataSubmiting] = useState(false);
-  const [createAnimating, setCreateAnimating] = useState(false);
-  const [confirmAnimating, setConfirmAnimating] = useState(false);
   const [errorModalVisible, setErrorModalVisible] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [inputVisible, setInputVisible] = useState(false);
 
   const { firebase } = useContext(AuthContext);
-
-  const creatListToogler = (e) => {
-    if (
-      e.target.classList.value.includes("Abort") ||
-      e.target.classList.value.includes("CreateIcon")
-    ) {
-      setInputVisible((prev) => !prev);
-    }
-  };
 
   const showErrorModal = (errorText) => {
     document.body.style.overflow = "hidden";
@@ -84,67 +76,20 @@ export default function WatchListContainer({ user, watchListPopupVisible }) {
             <>
               {data.length > 0 &&
                 data.map((item, i) => (
-                  <WatchList.ItemContainer key={item.name}>
-                    <WatchList.Number>{i + 1}</WatchList.Number>
-                    <WatchList.Name>{item.name.toUpperCase()}</WatchList.Name>
-                    <WatchList.Date></WatchList.Date>
-                    <WatchList.IconsWrapper>
-                      <WatchList.Add />
-                      <WatchList.Remove onClick={(e) => deleteListSubmit(e)} />
-                    </WatchList.IconsWrapper>
-                  </WatchList.ItemContainer>
-                ))}
-              <WatchList.ItemContainer createitem={1}>
-                <CSSTransition
-                  in={!inputVisible && !confirmAnimating}
-                  appear={true}
-                  timeout={{ enter: 300, exit: 300 }}
-                  unmountOnExit
-                  classNames="fade"
-                >
-                  <WatchList.Placeholder>CREATE LIST</WatchList.Placeholder>
-                </CSSTransition>
-                <CSSTransition
-                  in={inputVisible && !createAnimating}
-                  appear={true}
-                  timeout={{ enter: 300, exit: 300 }}
-                  unmountOnExit
-                  classNames="fade"
-                >
-                  <WatchList.Input
-                    placeholder={"Enter a name"}
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
+                  <WatchListItemContainer
+                    key={item.name}
+                    item={item}
+                    i={i}
+                    deleteListSubmit={deleteListSubmit}
                   />
-                </CSSTransition>
-                <CSSTransition
-                  in={inputVisible && !createAnimating}
-                  appear={true}
-                  timeout={{ enter: 300, exit: 300 }}
-                  unmountOnExit
-                  classNames="fade"
-                  onEnter={() => setConfirmAnimating(true)}
-                  onExited={() => setConfirmAnimating(false)}
-                >
-                  <WatchList.CreateListIconsWrapper>
-                    <WatchList.Confirm onClick={(e) => createListSubmit(e)} />
-                    <WatchList.Abort onClick={creatListToogler} />
-                  </WatchList.CreateListIconsWrapper>
-                </CSSTransition>
-                <CSSTransition
-                  in={!inputVisible && !confirmAnimating}
-                  appear={true}
-                  timeout={{ enter: 300, exit: 300 }}
-                  unmountOnExit
-                  classNames="fade"
-                  onEnter={() => setCreateAnimating(true)}
-                  onExited={() => setCreateAnimating(false)}
-                >
-                  <WatchList.CreateListIconsWrapper>
-                    <WatchList.CreateIcon onClick={creatListToogler} />
-                  </WatchList.CreateListIconsWrapper>
-                </CSSTransition>
-              </WatchList.ItemContainer>
+                ))}
+              <WatchListCreateItemContainer
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+                setInputVisible={setInputVisible}
+                inputVisible={inputVisible}
+                createListSubmit={createListSubmit}
+              />
             </>
           )}
         </WatchList>
---
+++ src/containers/auxillary-containers/containers/watch-list-items-container.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
+import { WatchList } from "../../../components";
+
+export function WatchListItemContainer({ item, i, deleteListSubmit }) {
+  return (
+    <WatchList.ItemContainer>
+      <WatchList.Number>{i + 1}</WatchList.Number>
+      <WatchList.Name>{item.name.toUpperCase()}</WatchList.Name>
+      <WatchList.Date></WatchList.Date>
+      <WatchList.IconsWrapper>
+        <WatchList.Add />
+        <WatchList.Remove onClick={(e) => deleteListSubmit(e)} />
+      </WatchList.IconsWrapper>
+    </WatchList.ItemContainer>
+  );
+}
+
+export function WatchListCreateItemContainer({
+  inputValue,
+  setInputValue,
+  setInputVisible,
+  inputVisible,
+  createListSubmit,
+}) {
+  const [createAnimating, setCreateAnimating] = useState(false);
+  const [confirmAnimating, setConfirmAnimating] = useState(false);
+
+  const creatListToogler = (e) => {
+    if (
+      e.target.classList.value.includes("Abort") ||
+      e.target.classList.value.includes("CreateIcon")
+    ) {
+      setInputVisible((prev) => !prev);
+    }
+  };
+  return (
+    <WatchList.ItemContainer createitem={1}>
+      <CSSTransition
+        in={!inputVisible && !confirmAnimating}
+        appear={true}
+        timeout={{ enter: 300, exit: 300 }}
+        unmountOnExit
+        classNames="fade"
+      >
+        <WatchList.Placeholder>CREATE LIST</WatchList.Placeholder>
+      </CSSTransition>
+      <CSSTransition
+        in={inputVisible && !createAnimating}
+        appear={true}
+        timeout={{ enter: 300, exit: 300 }}
+        unmountOnExit
+        classNames="fade"
+      >
+        <WatchList.Input
+          placeholder={"Enter a name"}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </CSSTransition>
+      <CSSTransition
+        in={inputVisible && !createAnimating}
+        appear={true}
+        timeout={{ enter: 300, exit: 300 }}
+        unmountOnExit
+        classNames="fade"
+        onEnter={() => setConfirmAnimating(true)}
+        onExited={() => setConfirmAnimating(false)}
+      >
+        <WatchList.CreateListIconsWrapper>
+          <WatchList.Confirm onClick={(e) => createListSubmit(e)} />
+          <WatchList.Abort onClick={creatListToogler} />
+        </WatchList.CreateListIconsWrapper>
+      </CSSTransition>
+      <CSSTransition
+        in={!inputVisible && !confirmAnimating}
+        appear={true}
+        timeout={{ enter: 300, exit: 300 }}
+        unmountOnExit
+        classNames="fade"
+        onEnter={() => setCreateAnimating(true)}
+        onExited={() => setCreateAnimating(false)}
+      >
+        <WatchList.CreateListIconsWrapper>
+          <WatchList.CreateIcon onClick={creatListToogler} />
+        </WatchList.CreateListIconsWrapper>
+      </CSSTransition>
+    </WatchList.ItemContainer>
+  );
+}

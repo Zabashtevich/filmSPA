28897569d67084bc5c180c4816865c193a76adda
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-09 11:09:20 +0300

    add: basic structure of utility modal comp

--- src/components/utility-modal/index.js
+++ src/components/utility-modal/index.js
@@ -1,5 +1,58 @@
 import React from "react";
 
+import {
+  Background,
+  Container,
+  ErrorIcon,
+  WarningIcon,
+  Title,
+  Message,
+  Wrapper,
+  AcceptButton,
+  DeclineButton,
+} from "./styles/utility-modal";
+
 export default function UtilityModal({ children, ...rest }) {
-  return <Container {...rest}>{children}</Container>;
+  return <Background {...rest}>{children}</Background>;
 }
+
+UtilityModal.Container = function UtilityModalContainer({ children, ...rest }) {
+  return <Container {...rest}>{children}</Container>;
+};
+
+UtilityModal.ErrorIcon = function UtilityModalErrorIcon({ children, ...rest }) {
+  return <ErrorIcon {...rest}>{children}</ErrorIcon>;
+};
+
+UtilityModal.WarningIcon = function UtilityModalWarningIcon({
+  children,
+  ...rest
+}) {
+  return <WarningIcon {...rest}>{children}</WarningIcon>;
+};
+
+UtilityModal.Title = function UtilityModalTitle({ children, ...rest }) {
+  return <Title {...rest}>{children}</Title>;
+};
+
+UtilityModal.Message = function UtilityModalMessage({ children, ...rest }) {
+  return <Message {...rest}>{children}</Message>;
+};
+
+UtilityModal.Wrapper = function UtilityModalWrapper({ children, ...rest }) {
+  return <Wrapper {...rest}>{children}</Wrapper>;
+};
+
+UtilityModal.AcceptButton = function UtilityModalAcceptButton({
+  children,
+  ...rest
+}) {
+  return <AcceptButton {...rest}>{children}</AcceptButton>;
+};
+
+UtilityModal.DeclineButton = function UtilityModalDeclineButton({
+  children,
+  ...rest
+}) {
+  return <DeclineButton {...rest}>{children}</DeclineButton>;
+};
--- src/components/utility-modal/styles/utility-modal.js
+++ src/components/utility-modal/styles/utility-modal.js
@@ -1,3 +1,22 @@
 import styled from "styled-components/macro";
 
+import { IoWarningOutline } from "react-icons/io5";
+import { BiErrorAlt } from "react-icons/bi";
+
+export const Background = styled.div``;
+
 export const Container = styled.div``;
+
+export const ErrorIcon = styled(BiErrorAlt)``;
+
+export const WarningIcon = styled(IoWarningOutline)``;
+
+export const ErrorIcon = styled.span``;
+
+export const Message = styled.span``;
+
+export const Wrapper = styled.div``;
+
+export const AcceptButton = styled.button``;
+
+export const DeclineButton = styled.button``;

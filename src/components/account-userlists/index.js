857d3fe978ffc685422d5c6f63ead824bcac525b
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-12 12:22:28 +0300

    add: account userlists comp basic structure

--- src/components/account-userlists/index.js
+++ src/components/account-userlists/index.js
@@ -1,1 +1,65 @@
 import React from "react";
+
+import {
+  Container,
+  List,
+  Thumbnail,
+  Wrapper,
+  Row,
+  Title,
+  Content,
+  Rename,
+  Delete,
+  Create,
+  Placeholder,
+} from "./styles/account-userlists";
+
+export default function AccountUserLists({ children, ...rest }) {
+  return <Container {...rest}>{children}</Container>;
+}
+
+AccountUserLists.List = function AccountUserListsList(children, ...rest) {
+  return <List {...rest}>{children}</List>;
+};
+
+AccountUserLists.Thumbnail = function AccountUserListsThumbnail(
+  children,
+  ...rest
+) {
+  return <Thumbnail {...rest}>{children}</Thumbnail>;
+};
+
+AccountUserLists.Wrapper = function AccountUserListsWrapper(children, ...rest) {
+  return <Wrapper {...rest}>{children}</Wrapper>;
+};
+
+AccountUserLists.Row = function AccountUserListsRow(children, ...rest) {
+  return <Row {...rest}>{children}</Row>;
+};
+
+AccountUserLists.Title = function AccountUserListsTitle(children, ...rest) {
+  return <Title {...rest}>{children}</Title>;
+};
+
+AccountUserLists.Content = function AccountUserListsContent(children, ...rest) {
+  return <Content {...rest}>{children}</Content>;
+};
+
+AccountUserLists.Rename = function AccountUserListsRename(children, ...rest) {
+  return <Rename {...rest}>{children}</Rename>;
+};
+
+AccountUserLists.Delete = function AccountUserListsDelete(children, ...rest) {
+  return <Delete {...rest}>{children}</Delete>;
+};
+
+AccountUserLists.Create = function AccountUserListsCreate(children, ...rest) {
+  return <Create {...rest}>{children}</Create>;
+};
+
+AccountUserLists.Placeholder = function AccountUserListsPlaceholder(
+  children,
+  ...rest
+) {
+  return <Placeholder {...rest}>{children}</Placeholder>;
+};
---
+++ src/components/account-userlists/styles/account-userlists.js
@@ -0,0 +1,23 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div``;
+
+export const List = styled.div``;
+
+export const Thumbnail = styled.div``;
+
+export const Wrapper = styled.div``;
+
+export const Row = styled.div``;
+
+export const Title = styled.div``;
+
+export const Content = styled.div``;
+
+export const Rename = styled.div``;
+
+export const Delete = styled.div``;
+
+export const Create = styled.div``;
+
+export const Placeholder = styled.div``;

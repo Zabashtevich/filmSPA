a132921ae656592e9b672c292123dd88c6d05cf4
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-14 12:37:59 +0300

    add: metalogic done

--- src/context/metalogic-context/context.js
+++ src/context/metalogic-context/context.js
@@ -1,29 +1,60 @@
 import React, { createContext, useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import useAuthListener from "../../hooks/useAuthListener";
+import { firebase } from "../../libs/firebase";
 import { useModalContext } from "../";
+import {
+  createListLogic,
+  removeLogic,
+  renameLogic,
+} from "../../utils/firebase-user-metalogic";
 
 export const MetalogicContext = createContext(null);
 
 export default function MetalogicContextProvider({ children }) {
   const userData = useSelector((store) => store.userData);
-
   const [modalstate, modalinterface] = useModalContext();
+  const [user, userLoading] = useAuthListener();
+
+  const { userlists } = userData;
   const { remove, rename, newlist } = modalstate;
   const { refreshModal } = modalinterface;
 
-  console.log(userData);
-
-  function removeList(id) {}
-
   useEffect(() => {
-    if (remove.accepted) {
+    if (remove.accepted && !userLoading) {
+      removeLogic(
+        firebase,
+        remove.target,
+        remove.id,
+        userlists,
+        user.displayName,
+      ).then(() => refreshModal());
     }
   }, [remove]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (rename.accepted && !userLoading) {
+      renameLogic(
+        firebase,
+        rename.id,
+        rename.name,
+        userlists,
+        user.displayName,
+      ).then(() => refreshModal());
+    }
+  }, [rename]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (newlist.accepted && !userLoading) {
+      createListLogic(
+        firebase,
+        newlist.name,
+        userlists,
+        user.displayName,
+      ).then(() => refreshModal());
+    }
+  }, []);
 
   return (
     <MetalogicContext.Provider value={[]}>{children}</MetalogicContext.Provider>
--- src/utils/firebase-user-metalogic.js
+++ src/utils/firebase-user-metalogic.js
@@ -1,1 +1,45 @@
-export function removeUserList() {}
+export function removeLogic(firebase, target, id, userlists, nickname) {
+  switch (target) {
+    case "list":
+      return removeList(firebase, target, id, userlists, nickname);
+  }
+}
+
+const removeList = (firebase, target, id, userlists, nickname) => {
+  return firebase
+    .firestore()
+    .collection(`${nickname}`)
+    .doc("collection")
+    .update({ list: [...userlists.filter((item) => +item.id !== id)] });
+};
+
+export function renameLogic(firebase, id, listname, userlists, nickname) {
+  return firebase
+    .firestore()
+    .collection(`${nickname}`)
+    .doc("collection")
+    .update({
+      list: [
+        ...userlists.map((item) =>
+          item.id === id ? { ...item, name: listname } : { ...item },
+        ),
+      ],
+    });
+}
+
+export function createListLogic(firebase, listname, userlists, nickname) {
+  return firebase
+    .firestore()
+    .collection(`${nickname}`)
+    .doc("collection")
+    .update({
+      list: [
+        ...userlists,
+        {
+          name: listname,
+          id: Math.floor(Math.random() * Math.floor(1000)),
+          content: [],
+        },
+      ],
+    });
+}

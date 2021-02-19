29f78e210a3c7785d211009e40181d0e195b0008
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-19 03:13:39 +0300

    add: useRate custom hook

---
+++ src/hooks/useRate/index.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import { firebase } from "./../../libs/firebase";
+import { useModalContext } from "./../../context";
+
+export default function useRate(nickname) {
+  const [value, setValue] = useState(null);
+
+  const userData = useSelector((state) => state.userData);
+  const [, modalController] = useModalContext();
+
+  const { loading } = userData;
+  const {
+    showProcessingWindow,
+    closeProcessingWindow,
+    showErorModal,
+  } = modalController;
+
+  useEffect(() => {
+    if (!loading && value !== null) {
+      showProcessingWindow("Processing your estimate");
+      firebase
+        .firestore()
+        .collection(nickname)
+        .doc("moviesrated")
+        .update({ list: ratedMovies.concat(value) })
+        .then(() => {
+          closeProcessingWindow();
+          setValue(null);
+        })
+        .catch(() => {
+          closeProcessingWindow();
+          showErorModal("Something gone wrong :c");
+          setValue(null);
+        });
+    }
+  }, [loading, value]);
+
+  return [setValue];
+}
--- src/reducers/metalogic/index.js
+++
@@ -1,8 +0,0 @@
-const initialState = { type: null, id: null, name: null, item: null };
-
-const metalogicReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    default:
-      return state;
-  }
-};

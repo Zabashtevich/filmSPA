43c281f49ea6a85bc4361a693cfdf7186e29d1f8
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-21 01:20:22 +0300

    extend: snippet structure, import icons

--- src/components/snippet/index.js
+++ src/components/snippet/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 
-import { Container, Element, Favorite, Userlist } from "./styles/snippet";
+import {
+  Container,
+  Element,
+  Favorite,
+  Userlist,
+  Backdrop,
+  Item,
+  Number,
+  Name,
+  Wrapper,
+  Add,
+  Remove,
+  Edit,
+  Popup,
+  Title,
+  Input,
+  Confirm,
+  Cancel,
+} from "./styles/snippet";
 
 export default function Snippet({ children, ...rest }) {
   return <Container {...rest}>{children}</Container>;
@@ -16,4 +34,56 @@ Snippet.Favorite = function SnippetFavorite({ ...rest }) {
 
 Snippet.Userlist = function SnippetUserlist({ ...rest }) {
   return <Userlist {...rest} />;
+};
+
+Snippet.Backdrop = function SnippetBackdrop({ children, ...rest }) {
+  return <Backdrop {...rest}>{children}</Backdrop>;
+};
+
+Snippet.Item = function SnippetItem({ children, ...rest }) {
+  return <Item {...rest}>{children}</Item>;
+};
+
+Snippet.Number = function SnippetNumber({ children, ...rest }) {
+  return <Number {...rest}>{children}</Number>;
+};
+
+Snippet.Name = function SnippetName({ children, ...rest }) {
+  return <Name {...rest}>{children}</Name>;
+};
+
+Snippet.Wrapper = function SnippetWrapper({ children, ...rest }) {
+  return <Wrapper {...rest}>{children}</Wrapper>;
+};
+
+Snippet.Add = function SnippetAdd({ ...rest }) {
+  return <Add {...rest} />;
+};
+
+Snippet.Remove = function SnippetRemove({ ...rest }) {
+  return <Remove {...rest} />;
+};
+
+Snippet.Edit = function SnippetEdit({ ...rest }) {
+  return <Edit {...rest} />;
+};
+
+Snippet.Popup = function SnippetPopup({ children, ...rest }) {
+  return <Popup {...rest}>{children}</Popup>;
+};
+
+Snippet.Title = function SnippetTitle({ children, ...rest }) {
+  return <Title {...rest}>{children}</Title>;
+};
+
+Snippet.Input = function SnippetInput({ ...rest }) {
+  return <Input {...rest} />;
+};
+
+Snippet.Confirm = function SnippetConfirm({ ...rest }) {
+  return <Confirm {...rest} />;
+};
+
+Snippet.Cancel = function SnippetCancel({ ...rest }) {
+  return <Cancel {...rest} />;
 };
--- src/components/snippet/styles/snippet.js
+++ src/components/snippet/styles/snippet.js
@@ -2,6 +2,11 @@ import styled from "styled-components/macro";
 
 import { AiFillHeart } from "react-icons/ai";
 import { RiPlayList2Fill } from "react-icons/ri";
+import { VscDiffAdded } from "react-icons/vsc";
+import { GoDiffRemoved } from "react-icons/go";
+import { FiEdit } from "react-icons/fi";
+import { AiFillCheckSquare } from "react-icons/ai";
+import { AiFillCloseSquare } from "react-icons/ai";
 
 export const Container = styled.div``;
 
@@ -10,3 +15,29 @@ export const Element = styled.span``;
 export const Favorie = styled(AiFillHeart)``;
 
 export const Userlist = styled(RiPlayList2Fill)``;
+
+export const Backdrop = styled.div``;
+
+export const Item = styled.div``;
+
+export const Number = styled.span``;
+
+export const Name = styled.span``;
+
+export const Wrapper = styled.div``;
+
+export const Add = styled(VscDiffAdded)``;
+
+export const Remove = styled(GoDiffRemoved)``;
+
+export const Edit = styled(FiEdit)``;
+
+export const Popup = styled.div``;
+
+export const Title = styled.span``;
+
+export const Input = styled.input``;
+
+export const Confirm = styled(AiFillCheckSquare)``;
+
+export const Cancel = styled(AiFillCloseSquare)``;

4d620982229654ced594a2bee3c1c78f29569ad6
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-21 19:15:50 +0300

    refactor: account root deleting old logic because of hooks refactor

--- src/containers/account-root-container/auxillary/index.js
+++
@@ -1,3 +0,0 @@
-export { default as UserlistContainer } from "./items/userlist";
-export { default as UserlistPlaceholder } from "./items/placeholder";
-export { default as UserlistCreatelist } from "./items/createlist";

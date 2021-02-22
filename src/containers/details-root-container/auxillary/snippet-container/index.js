0fac1c80e40483f7412aebd1ad34fe89c95edaea
Zabashtevich <vlados.zabashta@mail.ru>
2021-02-22 06:33:57 +0300

    refactor: delete snippet item aux cont

--- src/containers/details-root-container/auxillary/snippet-container/index.js
+++ src/containers/details-root-container/auxillary/snippet-container/index.js
@@ -5,12 +5,51 @@ import SnippetPopup from "./item";
 
 export default function SnipperContainer() {
   const [popupVisible, setPopupVisible] = useState(false);
+  const [editProps, setEditProps] = useState({
+    mounted: false,
+    item: { name: null, id: null },
+  });
+  const { mounted, item } = editProps;
 
   return (
     <Snippet>
       <Snippet.Element onClick={() => setPopupVisible(true)}>
         <Snippet.Userlist />
-        <SnippetPopup visible={popupVisible} />
+        <Snippet.Backdrop visible={popupVisible}>
+          <Snippet.Create visible={mounted}>
+            <Snippet.Title>EDIT NAME OF LIST '{`${item.name}`}'</Snippet.Title>
+            <Snippet.Wrapper>
+              <Snippet.Input />
+              <Snippet.Button borderactive={1}>CONFIRM</Snippet.Button>
+              <Snippet.Button borderactive={1}>CANCEL</Snippet.Button>
+            </Snippet.Wrapper>
+          </Snippet.Create>
+          {userlists.length === 0 && (
+            <Snippet.Item>
+              <Snippet.Placeholder />
+            </Snippet.Item>
+          )}
+          {userlists.length > 0 &&
+            userlists.map(({ name, id }, index) => {
+              return (
+                <Snippet.Item>
+                  <Snippet.Number>{index + 1}</Snippet.Number>
+                  <Snippet.Name>{name}</Snippet.Name>
+                  <Snippet.Inner>
+                    <Snippet.Button
+                      borderleft={1}
+                      onClick={() =>
+                        setEditProps({ mounted: true, item: { name, id } })
+                      }
+                    >
+                      EDIT
+                    </Snippet.Button>
+                    <Snippet.Button borderleft={1}>ADD</Snippet.Button>
+                  </Snippet.Inner>
+                </Snippet.Item>
+              );
+            })}
+        </Snippet.Backdrop>
       </Snippet.Element>
       <Snippet.Element>
         <Snippet.Favorite />

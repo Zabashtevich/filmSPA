c0d37acf666e186a0db009c3d792627c5b111a60
Zabashtevich <vlados.zabashta@mail.ru>
2021-03-19 18:20:07 +0300

    refactor: delete process, modal comp, containers, contexts(need to remade)

--- src/context/modal-context/index.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { ModalContext } from "./context";
-
-export default function useModalContext() {
-  const [modalstate, modalinterface] = useContext(ModalContext);
-  return [modalstate, modalinterface];
-}
--- src/context/process-context/index.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ProcessContext } from "./context";
-
-export default function useProcessContext() {
-  const [state, logic] = useContext(ProcessContext);
-
-  return [state, logic];
-}

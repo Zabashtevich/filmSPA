f499e0faba2f53f49a0c0c67fe3fc63352355b0d
Zabashtevich <vlados.zabashta@mail.ru>
2021-03-29 03:44:54 +0300

    add: account container testing coverage done

--- src/test/containers/account/index.test.js
+++ src/test/containers/account/index.test.js
@@ -28,6 +28,18 @@ export default function renderWithRedux(
 }
 
 describe("Account container", () => {
+  it("render correctly while loading", () => {
+    const { getByTestId, queryByTestId, queryByAltText } = renderWithRedux(
+      <AccountContainer />,
+      {
+        initialState: { profile: null, profileLoading: true },
+      },
+    );
+    expect(getByTestId("account-container")).toBeTruthy();
+    expect(queryByTestId("account-column")).toBeNull();
+    expect(queryByAltText("profile image")).toBeNull();
+  });
+
   it("render user data correctly after loading", async () => {
     const afterLoadingState = {
       profile: {
@@ -38,12 +50,26 @@ describe("Account container", () => {
       profileLoading: false,
     };
 
-    const {} = renderWithRedux(<AccountContainer />, {
+    const {
+      getByText,
+      getByTestId,
+      getByRole,
+      getByAltText,
+      container,
+    } = renderWithRedux(<AccountContainer />, {
       initialState: {
         userProfile: {
           ...afterLoadingState,
         },
       },
     });
+    expect(getByTestId("account-container")).toBeTruthy();
+    expect(getByText("Zabashtevich")).toBeTruthy();
+    expect(getByRole("img")).toBeTruthy();
+    expect(getByAltText("profile image").src).toContain(
+      "https://firebasestorage.googleapis.com/v0/b/tmdb-api-63318.appspot.com/o/unnamed.jpg?alt=media&token=b405c3ee-ba51-4ffd-bc4f-f1cf91e544ae",
+    );
+    expect(getByText(/your profile activity/i)).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });

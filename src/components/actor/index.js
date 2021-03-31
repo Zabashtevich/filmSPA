b96eef773f9f76c71e3264763cd0e76c092182ef
Zabashtevich <vlados.zabashta@mail.ru>
2021-03-31 03:20:05 +0300

    add: actor new structure

--- src/components/actor/index.js
+++ src/components/actor/index.js
@@ -1,5 +1,49 @@
 import React from "react";
 
+import {
+  Container,
+  Column,
+  Poster,
+  Subtitle,
+  Row,
+  Title,
+  Fieldname,
+  Fieldvalue,
+  Overview,
+} from "./styles/actor";
+
 export default function Actor({ children, ...rest }) {
   return <Container {...rest}>{children}</Container>;
 }
+
+Actor.Column = function ActorColumn({ children, ...rest }) {
+  return <Column {...rest}>{children}</Column>;
+};
+
+Actor.Poster = function ActorPoster({ ...rest }) {
+  return <Poster {...rest} />;
+};
+
+Actor.Subtitle = function ActorSubtitle({ children, ...rest }) {
+  return <Subtitle {...rest}>{children}</Subtitle>;
+};
+
+Actor.Row = function ActorRow({ children, ...rest }) {
+  return <Row {...rest}>{children}</Row>;
+};
+
+Actor.Fieldname = function ActorFieldname({ children, ...rest }) {
+  return <Fieldname {...rest}>{children}</Fieldname>;
+};
+
+Actor.Fieldvalue = function ActorFieldvalue({ children, ...rest }) {
+  return <Fieldvalue {...rest}>{children}</Fieldvalue>;
+};
+
+Actor.Title = function ActorTitle({ children, ...rest }) {
+  return <Title {...rest}>{children}</Title>;
+};
+
+Actor.Overview = function ActorOverview({ children, ...rest }) {
+  return <Overview {...rest}>{children}</Overview>;
+};
--- src/containers/actor/index.js
+++ src/containers/actor/index.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+import { Actor } from "../../components";
+
 export default function ActorContainer() {
-  return;
+  return (
+    <Actor>
+      <Actor.Column>
+        <Actor.Poster />
+        <Actor.Subtitle></Actor.Subtitle>
+        <Actor.Row>
+          <Actor.Fieldname></Actor.Fieldname>
+          <Actor.Fieldvalue></Actor.Fieldvalue>
+        </Actor.Row>
+      </Actor.Column>
+      <Actor.Column>
+        <Actor.Title></Actor.Title>
+        <Actor.Subtitle></Actor.Subtitle>
+        <Actor.Overview></Actor.Overview>
+      </Actor.Column>
+    </Actor>
+  );
 }

3fdec4f3f85e880a5acf9ce19170a876b316b5cd
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-07 18:56:17 +0300

    add: signup tests done

--- src/test/containers/auth/login.test.js
+++ src/test/containers/auth/login.test.js
@@ -6,6 +6,7 @@ import { AuthContainer } from "./../../../containers";
 import { ModalContext } from "../../../context/modal-context/context";
 import { AuthContext } from "../../../context/auth-context/context";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
 
 function renderComponent(mockAuthInterface, mockModalInterface) {
   return {
@@ -27,20 +28,21 @@ function renderComponent(mockAuthInterface, mockModalInterface) {
 
 describe("Auth container", () => {
   const authInterface = {
-    login: jest.fn(),
+    login: jest.fn().mockReturnValue(() => Promise.resolve(123)),
     loadUserAvatar: jest.fn(),
     signup: jest.fn(),
   };
 
-  const showErrorModal = jest.fn();
-  const showErrorList = jest.fn();
+  const modalInterface = {
+    showErrorModal: jest.fn(),
+    showErrorList: jest.fn(),
+  };
 
   it("correctly rendering login page with 'login' slug in path", () => {
-    const {
-      getByRole,
-      getByPlaceholderText,
-      getByText,
-    } = renderComponent(authInterface, { showErrorModal, showErrorList });
+    const { getByRole, getByPlaceholderText, getByText } = renderComponent(
+      authInterface,
+      modalInterface,
+    );
 
     expect(getByRole("form")).toBeTruthy();
     expect(getByPlaceholderText(/Email/i)).toBeTruthy();
@@ -53,21 +55,84 @@ describe("Auth container", () => {
   });
 
   it("correctly show error modal with missing user's data", async () => {
-    const { getByText } = renderComponent(authInterface, {
-      showErrorModal,
-      showErrorList,
-    });
+    const { getByText } = renderComponent(authInterface, modalInterface);
 
     expect(getByText(/LOGIN/)).toBeTruthy();
     await act(async () => {
       userEvent.click(getByText(/LOGIN/));
     });
-    expect(showErrorList).toHaveBeenCalled();
-    expect(showErrorList).toHaveBeenCalledTimes(1);
-    expect(showErrorList).toHaveBeenCalledWith([
+    expect(modalInterface.showErrorList).toHaveBeenCalled();
+    expect(modalInterface.showErrorList).toHaveBeenCalledTimes(1);
+    expect(modalInterface.showErrorList).toHaveBeenCalledWith([
       "Email field can not be empty",
       "Password field can not be empty",
     ]);
+  });
+
+  it("correctly sumbit form with filled user's inputs", async () => {
+    const authInterface = {
+      login: jest.fn().mockResolvedValue("Mock resolved data"),
+      loadUserAvatar: jest.fn(),
+      signup: jest.fn(),
+    };
+    const { getByText, getByPlaceholderText } = renderComponent(
+      authInterface,
+      modalInterface,
+    );
+    act(() => {
+      fireEvent.change(getByPlaceholderText(/Email/i), {
+        target: { value: "vlados.zabashta@mail.ru" },
+      });
+      fireEvent.change(getByPlaceholderText(/Password/i), {
+        target: { value: "12381238" },
+      });
+    });
+
+    expect(getByPlaceholderText(/Email/i).value).toContain(
+      "vlados.zabashta@mail.ru",
+    );
+    expect(getByPlaceholderText(/Password/i).value).toContain("12381238");
+
+    await act(async () => {
+      userEvent.click(getByText(/LOGIN/));
+    });
+    expect(authInterface.login).toHaveBeenLastCalledWith({
+      password: "12381238",
+      email: "vlados.zabashta@mail.ru",
+    });
+  });
+
+  it("correctly handles signup errprs", async () => {
+    const authInterface = {
+      login: jest.fn().mockRejectedValue({ message: "Mock rejected data" }),
+      loadUserAvatar: jest.fn(),
+      signup: jest.fn(),
+    };
+
+    const { getByText, getByPlaceholderText } = renderComponent(
+      authInterface,
+      modalInterface,
+    );
+    act(() => {
+      fireEvent.change(getByPlaceholderText(/Email/i), {
+        target: { value: "vlados.zabashta@mail.ru" },
+      });
+      fireEvent.change(getByPlaceholderText(/Password/i), {
+        target: { value: "12381238" },
+      });
+    });
+
+    expect(getByPlaceholderText(/Email/i).value).toContain(
+      "vlados.zabashta@mail.ru",
+    );
+    expect(getByPlaceholderText(/Password/i).value).toContain("12381238");
+
+    await act(async () => {
+      userEvent.click(getByText(/LOGIN/));
+    });
+    expect(modalInterface.showErrorModal).toHaveBeenLastCalledWith(
+      "Mock rejected data",
+    );
   });
 });
 

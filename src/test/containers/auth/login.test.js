b54b4809b510be37198f6e98bf5e9d5faea34680
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-07 17:03:18 +0300

    extend: auth tests

--- src/test/containers/auth/login.test.js
+++ src/test/containers/auth/login.test.js
@@ -1,27 +1,25 @@
-import { render } from "@testing-library/react";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
 import { MemoryRouter, Route } from "react-router";
 import { ThemeProvider } from "styled-components";
 import theme from "./../../../theme/theme";
 import { AuthContainer } from "./../../../containers";
 import { ModalContext } from "../../../context/modal-context/context";
-import { FirebaseContext } from "../../../context/firebase-context/context";
 import { AuthContext } from "../../../context/auth-context/context";
+import userEvent from "@testing-library/user-event";
 
-function renderComponent(authInterface) {
+function renderComponent(mockAuthInterface, mockModalInterface) {
   return {
     ...render(
       <ThemeProvider theme={theme}>
-        <FirebaseContext.Provider value={{}}>
-          <AuthContext.Provider value={authInterface}>
-            <ModalContext.Provider value={[, { showModal: () => {} }]}>
-              <MemoryRouter
-                initialEntries={[{ pathname: "/authentication/login" }]}
-              >
-                <Route component={AuthContainer} path="/authentication/:slug" />
-              </MemoryRouter>
-            </ModalContext.Provider>
-          </AuthContext.Provider>
-        </FirebaseContext.Provider>
+        <AuthContext.Provider value={mockAuthInterface}>
+          <ModalContext.Provider value={[null, mockModalInterface]}>
+            <MemoryRouter
+              initialEntries={[{ pathname: "/authentication/login" }]}
+            >
+              <Route component={AuthContainer} path="/authentication/:slug" />
+            </MemoryRouter>
+          </ModalContext.Provider>
+        </AuthContext.Provider>
       </ThemeProvider>,
     ),
   };
@@ -34,10 +32,16 @@ describe("Auth container", () => {
     signup: jest.fn(),
   };
 
-  it("correctly rendering login page with login slug in path", () => {
-    const { getByRole, getByPlaceholderText, getByText } = renderComponent(
-      authInterface,
-    );
+  const showErrorModal = jest.fn();
+  const showErrorList = jest.fn();
+
+  it("correctly rendering login page with 'login' slug in path", () => {
+    const {
+      getByRole,
+      getByPlaceholderText,
+      getByText,
+    } = renderComponent(authInterface, { showErrorModal, showErrorList });
+
     expect(getByRole("form")).toBeTruthy();
     expect(getByPlaceholderText(/Email/i)).toBeTruthy();
     expect(getByPlaceholderText(/Password/i)).toBeTruthy();
@@ -46,6 +50,24 @@ describe("Auth container", () => {
     expect(getByText(/back/i)).toBeTruthy();
     expect(getByText(/do not have an account/i)).toBeTruthy();
     expect(getByText(/create it here/i)).toBeTruthy();
+  });
+
+  it("correctly show error modal with missing user's data", async () => {
+    const { getByText } = renderComponent(authInterface, {
+      showErrorModal,
+      showErrorList,
+    });
+
+    expect(getByText(/LOGIN/)).toBeTruthy();
+    await act(async () => {
+      userEvent.click(getByText(/LOGIN/));
+    });
+    expect(showErrorList).toHaveBeenCalled();
+    expect(showErrorList).toHaveBeenCalledTimes(1);
+    expect(showErrorList).toHaveBeenCalledWith([
+      "Email field can not be empty",
+      "Password field can not be empty",
+    ]);
   });
 });
 

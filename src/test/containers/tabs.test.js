747a763c661de46d34fc4f13006a5bde941cd94b
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-12 18:59:16 +0300

    add: tabs tests done

--- src/test/containers/tabs.test.js
+++ src/test/containers/tabs.test.js
@@ -1,7 +1,9 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { TabsContainer } from "../../containers";
 import theme from "../../theme/theme";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 function renderComponent(component) {
   return {
@@ -29,5 +31,53 @@ describe("Tabs container", () => {
     expect(getByText("First title")).toBeTruthy();
     expect(getByText("Second title")).toBeTruthy();
     expect(getByText("Third title")).toBeTruthy();
+  });
+
+  it("has correst styles on anchors", () => {
+    const { getByText } = renderComponent(
+      <TabsContainer
+        tabs={initialTabs}
+        setActiveTab={setActiveTab}
+        active={"first_value"}
+      />,
+    );
+
+    expect(getByText("First title")).toHaveStyle("background-color: white");
+    expect(getByText("First title")).toHaveStyle(
+      "border-top-left-radius: 0.4rem",
+    );
+    expect(getByText("First title")).toHaveStyle(
+      "border-bottom-left-radius: 0.4rem",
+    );
+
+    expect(getByText("Second title")).toHaveStyle("background-color: #1f1f1f");
+
+    expect(getByText("Third title")).toHaveStyle("background-color: #1f1f1f");
+    expect(getByText("Third title")).toHaveStyle(
+      "border-top-right-radius: 0.4rem",
+    );
+    expect(getByText("Third title")).toHaveStyle(
+      "border-bottom-right-radius: 0.4rem",
+    );
+  });
+
+  it("contains correct working login", () => {
+    const { getByText } = renderComponent(
+      <TabsContainer
+        tabs={initialTabs}
+        setActiveTab={setActiveTab}
+        active={"first_value"}
+      />,
+    );
+
+    act(() => userEvent.click(getByText("Second title")));
+    expect(setActiveTab).toHaveBeenCalled();
+    expect(setActiveTab).toHaveBeenCalledTimes(1);
+    expect(setActiveTab).toHaveBeenCalledWith("second_value");
+
+    act(() => userEvent.click(getByText("Third title")));
+    expect(setActiveTab).toHaveBeenCalled();
+    expect(setActiveTab).toHaveBeenCalledTimes(2);
+    expect(setActiveTab).toHaveBeenCalledWith("third_value");
   });
 });

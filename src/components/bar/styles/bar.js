fc6388c058a5a31f1a232c7faafc00087e4a2010
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-16 03:11:38 +0300

    add: bar component structure

---
+++ src/components/bar/styles/bar.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div``;
+
+export const Inner = styled.div``;
+
+export const Input = styled.div``;
+
+export const Magnifier = styled.div``;
+
+export const Close = styled.div``;
+
+export const List = styled.div``;
+
+export const Item = styled.div``;
+
+export const Subtitle = styled.div``;
+
+export const Placeholder = styled.div``;
+
+export const Backdrop = styled.div``;

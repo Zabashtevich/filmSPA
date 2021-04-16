27e6749b418796ec746ee4395b4c56d29094e6ee
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-17 01:58:10 +0300

    add: header styles

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,26 +1,85 @@
 import styled from "styled-components/macro";
+import { Link as ReactRouterLink } from "react-router-dom";
 
-export const Container = styled.div``;
+import { BsSearch, BsX } from "react-icons/bs";
 
-export const Nav = styled.div``;
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.main};
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  display: flex;
+  z-index: 3000;
+  height: 75px;
+  width: 100%;
+  left: 0;
+  top: 0;
+`;
 
-export const Logo = styled.div``;
+export const Nav = styled.div`
+  font-weight: ${({ theme }) => theme.font.aux.weight.bold};
+  align-items: center;
+  margin-left: 5rem;
+  display: flex;
+  color: white;
+`;
 
-export const Wrapper = styled.div``;
+export const Logo = styled(ReactRouterLink)`
+  font-size: ${({ theme }) => theme.font.aux.size.huge};
+  text-decoration: none;
+  position: relative;
+  margin-right: 6rem;
+  color: white;
 
-export const Link = styled.div``;
+  &::after {
+    transform: translateY(-50%);
+    background-color: white;
+    left: calc(100% + 8px);
+    border-radius: 0.5rem;
+    position: absolute;
+    height: 15px;
+    content: "";
+    width: 50px;
+    top: 50%;
+  }
+`;
 
-export const Button = styled.div``;
+export const Wrapper = styled.div`
+  align-items: center;
+  margin-right: 8rem;
+  display: flex;
+`;
 
-export const Search = styled.div``;
+export const Link = styled(ReactRouterLink)`
+  text-decoration: none;
+  margin-right: 1rem;
+  font-size: 1rem;
+  color: white;
+`;
 
-export const Close = styled.div``;
+export const Button = styled.div`
+  font-size: ${({ theme }) => theme.font.aux.size.big};
+  ${({ theme }) => theme.animations.fade(200, 200, 200)};
+  margin: 0 2rem;
+  display: flex;
+  color: white;
+`;
 
-export const Search = styled.div``;
+export const Search = styled(BsSearch)``;
+
+export const Close = styled(BsX)``;
 
 export const Profile = styled.div``;
 
-export const Avatar = styled.div``;
+export const Avatar = styled.img`
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  border-radius: 50%;
+  height: 50px;
+`;
+
+export const Popup = styled.div`
+  display: none;
+`;
 
 export const Nickname = styled.div``;
 
--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,78 +1,57 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
+import { checkObject } from "./../../utils";
 import { Header } from "../../components";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 export default function HeaderContainer() {
-  const [headerVisible, setHeaderVisible] = useState(true);
-  const [active, setActive] = useState(false);
+  const [searchActive, setSearchActive] = useState(false);
   const [popupVisible, setPopupVisible] = useState(false);
   const { profileLoading, profile } = useSelector((state) => state.userProfile);
-  let prevScrollPosition = 0;
-  function inputToggler() {
-    setActive((prev) => !prev);
-  }
 
-  function headerToggler() {
-    const current = window.pageYOffset;
+  // function headerToggler() {
+  //   const current = window.pageYOffset;
 
-    if (prevScrollPosition > current) {
-      setHeaderVisible(true);
-    } else {
-      setHeaderVisible(false);
-    }
-    prevScrollPosition = current;
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", headerToggler);
-    return () => window.removeEventListener("scroll", headerToggler);
-  }, []);
-
-  useEffect(() => {
-    if (popupVisible) {
-      window.addEventListener("click", () => setPopupVisible(false));
-    }
-    return () => window.removeEventListener("click", this);
-  }, [popupVisible]);
-
-  const profilevisible = !profileLoading && profile !== null;
+  //   if (prevScrollPosition > current) {
+  //     setHeaderVisible(true);
+  //   } else {
+  //     setHeaderVisible(false);
+  //   }
+  //   prevScrollPosition = current;
+  // }
 
   return (
-    <Header visible={headerVisible}>
+    <Header>
+      <Header.Nav>
+        <Header.Logo to="/">TMDB</Header.Logo>
+        <Header.Link to="/trending/movie">Trending</Header.Link>
+        <Header.Link to="/search">Search</Header.Link>
+      </Header.Nav>
       <Header.Wrapper>
-        <Header.Logo>TMDB</Header.Logo>
-        <Header.Link to="/">MAIN</Header.Link>
-        <Header.Link to="/">SEARCH</Header.Link>
-        <Header.Link to="/">ACCOUNT</Header.Link>
-      </Header.Wrapper>
-      <Header.Wrapper>
-        <Header.Inner onClick={inputToggler}>
-          <Header.Input
-            onBlur={() => setActive(false)}
-            active={active ? 1 : 0}
-          />
-          <Header.Icon />
-        </Header.Inner>
-        <Header.Profile>
-          {profilevisible && (
+        {!profileLoading && checkObject.notEmty(profile) && (
+          <Header.Profile>
             <Header.Avatar
-              src={profile?.photoURL || "https://dummyimage.com/50x50/aaa/aaa"}
-              onClick={(e) => {
-                e.stopPropagation();
-                setPopupVisible((prev) => !prev);
-              }}
+              onClick={() => setPopupVisible((prev) => !prev)}
+              src={profile?.photoURL}
             />
-          )}
-          <Header.Popup visible={popupVisible}>
-            <Header.Info>
-              <Header.Name>{profile?.displayName}</Header.Name>
-              <Header.Mail>{profile?.email}</Header.Mail>
-            </Header.Info>
-            <Header.Account to="/account">To profile</Header.Account>
-            <Header.Signout>Sign Out</Header.Signout>
-          </Header.Popup>
-        </Header.Profile>
+            <Header.Popup visible={popupVisible}>
+              <Header.Nickname>{profile.displayName}</Header.Nickname>
+              <Header.Mail></Header.Mail>
+              <Header.Item to="">to Account</Header.Item>
+              <Header.Item>to Userlits</Header.Item>
+              <Header.Item>Logout</Header.Item>
+            </Header.Popup>
+          </Header.Profile>
+        )}
+        <SwitchTransition mode="out-in">
+          <CSSTransition key={searchActive} classNames="fade" timeout={200}>
+            <Header.Button onClick={() => setSearchActive((prev) => !prev)}>
+              {!searchActive && <Header.Search />}
+              {searchActive && <Header.Close />}
+            </Header.Button>
+          </CSSTransition>
+        </SwitchTransition>
       </Header.Wrapper>
     </Header>
   );

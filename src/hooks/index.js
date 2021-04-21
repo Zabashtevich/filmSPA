553badf0e2214b20a79b4e187c835680265f550c
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-22 01:04:22 +0300

    refactor: delete useTrending hook, extend useFetch

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,4 @@ export { default as useAuthListener } from "./useAuthListener";
 export { default as useFetch } from "./useFetch";
 export { default as useFirestore } from "./useFirestore";
 export { default as useEstimate } from "./useEstimate";
-export { default as useTrending } from "./useTrending";
 export { default as useSearch } from "./useSearch";
--- src/hooks/useFetch/index.js
+++ src/hooks/useFetch/index.js
@@ -1,39 +1,41 @@
 import { useState, useEffect } from "react";
+import { querryParams } from "../../constants/constants";
 
-export default function useFetch(type, target, querries = false) {
-  const [{ loading, list }, setData] = useState({
+export default function useFetch({
+  type,
+  target,
+  period = "",
+  querries = false,
+}) {
+  const [{ loading, data }, setData] = useState({
     loading: true,
     list: null,
   });
 
-  const querryParams = {
-    tv: "credits,recommendations,images,videos,reviews,account_states",
-    movie: "credits,recommendations,images,videos,reviews,account_states",
-    person: "combined_credits,images",
-  };
-
-  const appendToResponse =
-    (querries && `&&append_to_response=${querryParams[type]}`) || "";
+  const url = `https://api.themoviedb.org/3/${type}/${target}/${period}?${
+    querries ? querryParams[type] : ""
+  }&api_key=${process.env.REACT_APP_API_KEY}`;
 
   useEffect(() => {
     let mounted = true;
-    if (mounted) {
+    if (mounted && url) {
       setData((prev) => ({ ...prev, loading: true }));
-      fetch(
-        `https://api.themoviedb.org/3/${type}/${target}?api_key=${process.env.REACT_APP_API_KEY}${appendToResponse}`,
-      )
+      fetch(url)
         .then((response) => response.json())
         .then((data) => {
           if (data.success === false) {
           } else {
-            setData((prev) => ({ ...prev, loading: false, list: data }));
+            setData((prev) => ({ ...prev, loading: false, data }));
           }
         });
     }
     return () => {
       mounted = false;
     };
-  }, [type, target]);
+  }, [type, target, period, querries]);
 
-  return [list, loading];
+  return [
+    { data, loading },
+    { getDetails, getTrendings },
+  ];
 }

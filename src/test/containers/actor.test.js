07985a1b9046a9fdb1b94e9b73e83fa2b7c2143b
Zabashtevich <vlados.zabashta@mail.ru>
2021-04-28 21:59:16 +0300

    fix: actor tests

--- src/test/containers/actor.test.js
+++ src/test/containers/actor.test.js
@@ -1,25 +1,34 @@
 import { ThemeProvider } from "styled-components";
 import "@testing-library/jest-dom";
 import { MemoryRouter, Route } from "react-router";
+import { render } from "@testing-library/react";
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import { ActorContainer } from "../../containers";
 import theme from "./../../theme/theme";
 import { useFetch } from "../../hooks";
 import { useCreditsContext } from "../../context";
-import { render } from "@testing-library/react";
+import userData from "./../../reducers/user-data";
 
 jest.mock("./../../hooks", () => ({ useFetch: jest.fn() }));
 jest.mock("./../../context", () => ({ useCreditsContext: jest.fn() }));
 jest.mock("./../../containers/credits", () => () => <div />);
 
-function renderComponent() {
+function renderWithRedux({
+  initialState,
+  store = createStore(combineReducers({ userData }), initialState),
+}) {
   return {
     ...render(
-      <MemoryRouter initialEntries={[{ pathname: "/actor/123" }]}>
-        <ThemeProvider theme={theme}>
-          <Route component={ActorContainer} path="/actor/:slug" />
-        </ThemeProvider>
-      </MemoryRouter>,
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/actor/123" }]}>
+          <ThemeProvider theme={theme}>
+            <Route component={ActorContainer} path="/actor/:slug" />
+          </ThemeProvider>
+        </MemoryRouter>
+      </Provider>,
     ),
   };
 }
@@ -36,22 +45,38 @@ describe("Actor container", () => {
     place_of_birth: "dummy place",
     known_for_department: "dummy department",
   };
+  const loadingState = {
+    userDataLoading: true,
+    userDataExist: false,
+    loggedIn: false,
+    profile: null,
+    lists: { userlists: null, favorites: null, votes: null },
+  };
+  const initialState = {
+    userDataLoading: false,
+    userDataExist: true,
+    loggedIn: true,
+    profile: {},
+    lists: { userlists: [], favorites: [], votes: [] },
+  };
 
   it("displays skeleton on loading", () => {
     useFetch.mockReturnValue([null, true]);
     useCreditsContext.mockReturnValue([, setCredits]);
 
-    const { getByTestId } = renderComponent();
+    const { getByTestId } = renderWithRedux({
+      initialState: { userData: loadingState },
+    });
 
     expect(getByTestId(/actor-column-skeleton/i)).toBeTruthy();
     expect(getByTestId(/actor-content-skeleton/i)).toBeTruthy();
   });
 
-  it("calls useFetch witch expected params", () => {
+  it("calls useFetch with correct params", () => {
     useFetch.mockReturnValue([null, true]);
     useCreditsContext.mockReturnValue([, setCredits]);
 
-    const {} = renderComponent();
+    const {} = renderWithRedux({ initialState: { userData: loadingState } });
 
     expect(useFetch).toHaveBeenCalled();
     expect(useFetch).toHaveBeenCalledWith({
@@ -65,7 +90,9 @@ describe("Actor container", () => {
     useFetch.mockReturnValue([mockedData, false]);
     useCreditsContext.mockReturnValue([, setCredits]);
 
-    const { getByText, getByTestId } = renderComponent();
+    const { getByText, getByTestId } = renderWithRedux({
+      initialState: { userData: loadingState },
+    });
 
     expect(getByText(/dummy name/i)).toBeTruthy();
     expect(getByText(/overview/i)).toBeTruthy();
@@ -82,17 +109,17 @@ describe("Actor container", () => {
     );
   });
 
-  it("calls setCredits after getting data", () => {
-    useFetch.mockReturnValue([null, true]);
+  it("calls setCredits after getting data and loading profile", () => {
+    useFetch.mockReturnValue([mockedData, false]);
     useCreditsContext.mockReturnValue([, setCredits]);
 
-    const {} = renderComponent();
+    const {} = renderWithRedux({ initialState: { userData: initialState } });
 
     expect(setCredits).toHaveBeenCalled();
     expect(setCredits).toHaveBeenCalledTimes(1);
     expect(setCredits).toHaveBeenCalledWith({
-      creditsLoading: false,
-      array: [],
+      loading: false,
+      items: [],
     });
   });
 });

d636debc62c85c31bc71bcff67611366ad7af78a
Zabashtevich <vlados.zabashta@mail.ru>
2021-05-12 20:25:18 +0300

    add: collection cast responsive

--- src/components/collection-cast/styles/collection-cast.js
+++ src/components/collection-cast/styles/collection-cast.js
@@ -8,7 +8,7 @@ export const Inner = styled.div`
 
 export const Container = styled.div`
   border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-  padding: 2rem 0;
+  padding: 2rem 1rem;
 `;
 
 export const Title = styled.span`
@@ -27,12 +27,25 @@ export const Card = styled.div`
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
   border: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
   border-radius: 0.3rem;
   margin: 0.5rem 0.2rem;
   align-items: center;
   display: flex;
   width: 250px;
   height: 60px;
+
+  @media (max-width: 850px) {
+    width: 200px;
+    height: 60px;
+  }
+
+  @media (max-width: 500px) {
+    margin: 0 auto;
+    margin-top: 0.4rem;
+    width: 250px;
+    height: 60px;
+  }
 `;
 
 export const Poster = styled.img`
@@ -43,7 +56,16 @@ export const Poster = styled.img`
 `;
 
 export const Info = styled.div`
+  text-overflow: ellipsis;
+  flex-direction: column;
   margin-left: 0.2rem;
+  white-space: nowrap;
+  overflow: hidden;
+  display: flex;
+
+  @media (max-width: 650px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const Name = styled(ReactRouterLink)`
@@ -52,8 +74,6 @@ export const Name = styled(ReactRouterLink)`
   text-decoration: none;
   white-space: nowrap;
   overflow: hidden;
-  display: block;
-  width: 170px;
   color: black;
 
   &:hover {
@@ -67,6 +87,4 @@ export const Role = styled.span`
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
-  display: block;
-  width: 170px;
 `;

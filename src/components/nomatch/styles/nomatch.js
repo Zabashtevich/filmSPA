e0b4c3096b30d73b517c5d98dcbeaba090e722ef
Zabashtevich <vlados.zabashta@mail.ru>
2021-05-21 02:43:07 +0300

    add: nomatch media querries

--- src/components/nomatch/styles/nomatch.js
+++ src/components/nomatch/styles/nomatch.js
@@ -15,6 +15,10 @@ export const Message = styled.h1`
   font-weight: ${({ theme }) => theme.font.aux.weight.bold};
   font-size: ${({ theme }) => theme.font.aux.size.huge};
   color: rgba(0, 0, 0, 0.4);
+
+  @media (max-width: 700px) {
+    font-size: ${({ theme }) => theme.font.aux.size.large};
+  }
 `;
 
 export const Return = styled(ReactRouterLink)`
@@ -30,5 +34,9 @@ export const Return = styled(ReactRouterLink)`
     box-shadow: 0 0 0 2px #ffffff, 0 0 3px 5px #3f3f3f,
       rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     outline: none;
+  }
+
+  @media (max-width: 700px) {
+    font-size: ${({ theme }) => theme.font.aux.size.normal};
   }
 `;
